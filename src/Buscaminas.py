992bf4b5de5a1e5c08250dcefbe25cce9e2c095b
Renato <renatoatencioaedo@gmail.com>
2023-12-18 11:46:24 -0300

    Arreglo codigo, Mejora Diagramas

--- src/Buscaminas.py
+++ src/Buscaminas.py
@@ -13,8 +13,8 @@ class Buscaminas:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("Buscaminas")
-        #razon de 1/5 (minas/dimension)
-        self.size = 15
+        # razon de 1/5 (minas/dimension)
+        self.size = 20
         self.bomb_count = 45
         self.buttons = []
         self.is_game_over = False
@@ -24,12 +24,12 @@ class Buscaminas:
         self.place_bombs()
         self.calculate_numbers()
         self.create_board()
-   
-    # Este método inicializa la matriz que representa el tablero del juego con celdas vacías 
-    #(inicialmente todas las celdas tienen el valor 0).
+
+    # Este método inicializa la matriz que representa el tablero del juego con celdas vacías
+    # (inicialmente todas las celdas tienen el valor 0).
     def generate_board(self):
         self.board = [[0 for _ in range(self.size)] for _ in range(self.size)]
- 
+
     # Coloca las minas en posiciones aleatorias en el tablero. Se asegura de que las minas no se superpongan en la misma celda.
     def place_bombs(self):
         bombs_placed = 0
@@ -39,16 +39,15 @@ class Buscaminas:
             if self.board[x][y] != -1:
                 self.board[x][y] = -1
                 bombs_placed += 1
-  
-    # Calcula el número de minas adyacentes a cada celda que no contenga una mina. 
+
+    # Calcula el número de minas adyacentes a cada celda que no contenga una mina.
     # Para cada celda vacía, cuenta la cantidad de minas en su vecindad y actualiza ese número en la matriz del tablero.
     def calculate_numbers(self):
         for i in range(self.size):
             for j in range(self.size):
                 if self.board[i][j] != -1:
                     self.board[i][j] = self.count_adjacent_bombs(i, j)
-    
-   
+
     # Cuenta el número de minas adyacentes a una celda dada en el tablero.
     def count_adjacent_bombs(self, x, y):
         count = 0
@@ -62,7 +61,7 @@ class Buscaminas:
                     count += 1
         return count
 
-    #: Crea los botones en la interfaz gráfica. Configura los botones y los vincula con métodos específicos 
+    #: Crea los botones en la interfaz gráfica. Configura los botones y los vincula con métodos específicos
     # (click y place_flag) cuando se hace clic o se hace clic derecho (para colocar una bandera).
     def create_board(self):
         for i in range(self.size):
@@ -76,7 +75,7 @@ class Buscaminas:
                 row.append(button)
             self.buttons.append(row)
 
-    # Maneja el evento cuando se hace clic en una celda del tablero. Si la celda contiene una mina, revela todo el tablero y muestra 
+    # Maneja el evento cuando se hace clic en una celda del tablero. Si la celda contiene una mina, revela todo el tablero y muestra
     # "Game Over". De lo contrario, revela la celda y verifica si se ha ganado el juego.
     def click(self, x, y):
         if self.is_game_over or self.flags[x][y]:
@@ -90,10 +89,11 @@ class Buscaminas:
             if self.check_win():
                 self.game_won()
 
-    #Método recursivo que revela una celda y, si esa celda está vacía, revela recursivamente las celdas adyacentes vacías.
+
+    # Método recursivo que revela una celda y, si esa celda está vacía, revela recursivamente las celdas adyacentes vacías.
     def reveal_cell(self, x, y):
         if self.board[x][y] == 0:
-            self.buttons[x][y].config(text="", state=tk.DISABLED)
+            self.buttons[x][y].config(text="", state=tk.DISABLED, bg="lightgray")
             for i in range(-1, 2):
                 for j in range(-1, 2):
                     if (
@@ -103,9 +103,11 @@ class Buscaminas:
                     ):
                         self.reveal_cell(x + i, y + j)
         else:
-            self.buttons[x][y].config(text=self.board[x][y], state=tk.DISABLED)
+            self.buttons[x][y].config(
+                text=self.board[x][y], state=tk.DISABLED, bg="lightgray"
+            )
 
-    #Revela todas las celdas del tablero al finalizar el juego, mostrando las minas y los números.
+    # Revela todas las celdas del tablero al finalizar el juego, mostrando las minas y los números.
     def reveal_board(self):
         for i in range(self.size):
             for j in range(self.size):
@@ -128,7 +130,7 @@ class Buscaminas:
                     return False
         return True
 
-    #Maneja el final del juego mostrando "Game Over" en la interfaz y muestra un menú para reiniciar o salir.
+    # Maneja el final del juego mostrando "Game Over" en la interfaz y muestra un menú para reiniciar o salir.
     def game_over(self):
         self.is_game_over = True
         game_over_label = tk.Label(self.root, text="Game Over!", fg="red")
@@ -151,7 +153,7 @@ class Buscaminas:
         self.__init__()
         self.start()
 
-    #Cierra la ventana del juego
+    # Cierra la ventana del juego
     def quit_game(self):
         self.root.destroy()
 
@@ -162,13 +164,15 @@ class Buscaminas:
         game_won_label.grid(row=self.size + 1, columnspan=self.size)
         self.show_retry_menu()
 
-    #Coloca o quita una bandera en una celda, indicando la posible ubicación de una mina.
+    # Coloca o quita una bandera en una celda, indicando la posible ubicación de una mina.
+    # Coloca o quita una bandera en una celda, indicando la posible ubicación de una mina.
     def place_flag(self, x, y):
-        if not self.is_game_over:
+        if not self.is_game_over and self.buttons[x][y]["state"] != tk.DISABLED:
             if self.flags[x][y]:
                 self.buttons[x][y].config(text="", fg="black")
             else:
-                self.buttons[x][y].config(text="F", fg="blue")
+                if not self.flags[x][y]:
+                    self.buttons[x][y].config(text="F", fg="blue")
             self.flags[x][y] = not self.flags[x][y]
 
     def start(self):
